a71d5a7a7ff6307113fa1e389ad9b1b1a4cd8c79
Satyam <satus2311@gmail.com>
2023-03-17 17:25:30 +0530

    Added media query for responsive sidebar

--- src/dashboard/DashboardToggle.js
+++ src/dashboard/DashboardToggle.js
@@ -3,15 +3,29 @@ import { Button, Drawer } from 'rsuite';
 import { Icon } from '@rsuite/icons';
 import { useModalState } from '../misc/custom-hooks';
 import Dashboard from '.';
+import { useMediaQuery } from '../misc/custom-hooks';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 992px');
+
+  const drawerHandler = () => {
+    if (isMobile) {
+      return 'full';
+    }
+  };
+
   return (
     <>
       <Button block color='blue' appearance='primary' onClick={open}>
         <Icon as={BiBarChartSquare} size='1.3em' /> Dashboard
       </Button>
-      <Drawer open={isOpen} onClose={close} placement={'left'}>
+      <Drawer
+        size={drawerHandler()}
+        open={isOpen}
+        onClose={close}
+        placement={'left'}
+      >
         <Dashboard />
       </Drawer>
     </>
