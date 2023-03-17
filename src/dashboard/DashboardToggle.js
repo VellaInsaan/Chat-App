63c73560d14d8892b973b8e49dd3387f38015266
Satyam <satus2311@gmail.com>
2023-03-17 18:09:55 +0530

    Added signout button and display name of user on Dashboard

--- src/dashboard/DashboardToggle.js
+++ src/dashboard/DashboardToggle.js
@@ -4,10 +4,24 @@ import { Icon } from '@rsuite/icons';
 import { useModalState } from '../misc/custom-hooks';
 import Dashboard from '.';
 import { useMediaQuery } from '../misc/custom-hooks';
+import { useCallback } from 'react';
+import { auth } from '../misc/firebase';
+import { Notification, useToaster } from 'rsuite';
 
 const DashboardToggle = () => {
+  const toaster = useToaster();
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px');
+
+  const onSignOut = useCallback(() => {
+    const messageOnSignOut = (
+      <Notification type={'info'} header={'Signed Out'} />
+    );
+    auth.signOut();
+
+    toaster.push(messageOnSignOut, { placement: 'topCenter' });
+    close();
+  }, [close, toaster]);
 
   const drawerHandler = () => {
     if (isMobile) {
@@ -26,7 +40,7 @@ const DashboardToggle = () => {
         onClose={close}
         placement={'left'}
       >
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
