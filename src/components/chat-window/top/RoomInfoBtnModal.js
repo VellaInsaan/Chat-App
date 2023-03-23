27aa54f67e7f5b89419635166543018facd38f88
Satyam <satus2311@gmail.com>
2023-03-24 01:44:49 +0530

    Created model for room information

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,33 @@
+import { Button, Modal } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModalState } from '../../../misc/custom-hooks';
+
+const RoomInfoBtnModal = () => {
+  const { isOpen, open, close } = useModalState();
+  const description = useCurrentRoom((val) => val.description);
+  const name = useCurrentRoom((val) => val.name);
+
+  return (
+    <>
+      <Button appearance='link' className='px-0' onClick={open}>
+        Room Info
+      </Button>
+      <Modal open={isOpen} onClose={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className='mb-1'>Description </h6>
+          <p>{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,9 +1,45 @@
+import Icon from '@rsuite/icons/lib/Icon';
 import { memo } from 'react';
+import { RiArrowLeftCircleLine } from 'react-icons/ri';
+
 import { useCurrentRoom } from '../../../context/current-room.context';
+
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import { Button, ButtonToolbar } from 'rsuite';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const Top = () => {
   const name = useCurrentRoom((val) => val.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width: 992px');
+  return (
+    <div>
+      <div className='d-flex justify-content-between align-items-center'>
+        <h4>
+          <Link
+            to='/'
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          >
+            <Icon as={RiArrowLeftCircleLine} size='1.5em' />
+          </Link>
+          <span className='text-disappear'>{name}</span>
+        </h4>
+
+        <ButtonToolbar className='ws-nowrap'>
+          <Button>todo</Button>
+        </ButtonToolbar>
+      </div>
+
+      <div className='d-flex justify-content-between align-items-center'>
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(Top);
